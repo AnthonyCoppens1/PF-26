9b30ac698966d6c9d5ad8b292c31bb69dd31bbb1
AnthonyCoppens1 <anthonycoppens95@gmail.com>
2026-09-24 00:01:22 +0200

    First week of class - Hello World!

---
+++ Week01/IADI-1A/HelloWorld/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
+            
+            //This is some extra explanation that you can add in here
+
+            //Console.Write("Help me!");
+
+            /*Console.Write("I don't know what I'm doing???");
+            Console.WriteLine("TEST");*/
+
+            /* */
+
+            //FIRST DECLARE
+            string name;
+
+            //NEXT INITIALIZE
+            name = "Juliana";
+            string name2 = "Anthony";
+
+            //3 techniques
+            //number 1
+            Console.WriteLine("Hello " + name);
+
+            //number 2
+            Console.WriteLine($"Hello {name}");
+
+            //number 3
+            Console.WriteLine("Hello {0} and {1}", name, name2);
+
+
+
+            int number = 7;
+            int secondNumber = 12;
+
+            Console.WriteLine("Hello" + number);
+
+
+
+
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine($"Hello {Console.ReadLine()}");
+            
+
+        }
+    }
+}
---
+++ Week01/IADI-1B/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             //this is just an instruction no second line is possible
+            /*
+            This is text
+            this too
+            no worries
+            */
+
+
+            Console.WriteLine("What's your name? ");
+            string name = Console.ReadLine();
+            int number = 5;
+
+           //technique 1: concatenation
+            Console.WriteLine("Hello " + "senior " + number);
+
+            //second way
+            Console.WriteLine($"Hello {name}!");
+
+            //third way
+            /*name = "0";
+            number = 1;*/
+            Console.WriteLine("Hello {0} and {1}!", name, number);
+        }
+    }
+}
